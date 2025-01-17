430cc6f5e048787c26e97874ad5680fe57fba7fc
Jean <nguyen.jean@me.com>
2025-01-17 09:28:11 +0100

    add translate file

---
+++ src/assets/translations.js
@@ -0,0 +1,10 @@
+export const translations = {
+  en: {
+    welcome: 'Welcome',
+    generatePassword: 'Generate Password',
+  },
+  fr: {
+    welcome: 'Bienvenue',
+    generatePassword: 'Générer un mot de passe',
+  },
+};
