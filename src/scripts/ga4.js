1df5629cab572e0bfadf5ba517261cb0f2368b69
Jean <nguyen.jean@me.com>
2025-01-26 22:08:10 +0100

    add partytown and GA4

--- src/scripts/ga4.js
+++ src/scripts/ga4.js
@@ -1,6 +1,8 @@
+const GA4_ID = import.meta.env.VITE_GA4_ID;
+
 window.dataLayer = window.dataLayer || [];
 function gtag() {
   window.dataLayer.push(arguments);
 }
 gtag('js', new Date());
-gtag('config', 'G-PGE2567J9F');
+gtag('config', GA4_ID);
