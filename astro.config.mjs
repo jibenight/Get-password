eb6841f01c5fc5c27fa72dbe26d8f43fffd257dc
Jean <nguyen.jean@me.com>
2025-01-26 22:08:05 +0100

    add partytown and GA4

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,19 @@ import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://get-password.com/',
-  integrations: [sitemap()],
+  integrations: [
+    sitemap(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push', 'gtag'],
+      },
+    }),
+  ],
   vite: {
     resolve: {
       alias: {
---
+++ src/scripts/ga4.js
@@ -0,0 +1,6 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  window.dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'G-PGE2567J9F');
