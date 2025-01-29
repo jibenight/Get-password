7433211d08ebad67bb14f211a7eecb9657c6b961
Jean <nguyen.jean@me.com>
2025-01-29 22:01:42 +0100

    update ga

--- src/scripts/ga4.js
+++ src/scripts/ga4.js
@@ -1,8 +1,8 @@
-const GA4_ID = import.meta.env.VITE_GA4_ID;
+// const GA4_ID = import.meta.env.VITE_GA4_ID;
 
 window.dataLayer = window.dataLayer || [];
 function gtag() {
   window.dataLayer.push(arguments);
 }
 gtag('js', new Date());
-gtag('config', GA4_ID);
+gtag('config', 'G-PGE2567J9F');
