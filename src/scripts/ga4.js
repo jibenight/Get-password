8b72b816909ea6dc00e5e9cb01d45a4ec94cbf7d
Jean <nguyen.jean@me.com>
2025-02-01 23:07:09 +0100

    delete GA script for serveur side GA4

--- src/scripts/ga4.js
+++
@@ -1,8 +0,0 @@
-// const GA4_ID = import.meta.env.VITE_GA4_ID;
-
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-  window.dataLayer.push(arguments);
-}
-gtag('js', new Date());
-gtag('config', 'G-PGE2567J9F');
